ea932f6cb95518d9676a1be3a46d4b774b4521c3
Jake Annal <jxa8057@rit.edu>
2020-04-03 21:39:40 -0400

    Minor styling

--- src/components/Headers/H1/index.js
+++ src/components/Headers/H1/index.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 const H1 = styled.h1`
+  text-align: center;
   font-weight: 400;
-  margin: 0;
+  margin: 0 auto;
 `;
 
 export default H1;
--- src/components/Page/PageStyled.js
+++ src/components/Page/PageStyled.js
@@ -4,6 +4,7 @@ const Page = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
+  padding: 24px;
 `;
 
 export default Page;
